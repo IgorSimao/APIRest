802c6af5849fd1fe34a8bb9e37465e3399c8dd4f
Igor Simao <igor.simao@novaxs.com.br>
2023-08-30 20:05:38 -0300

    Fazendo os metodos simples de manipulação dos cadastros

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+let id = 2;
+
 let users = [
     {id: 1, nome: "Igor Simao", email: "igor.simao@gmail.com"},
     {id: 2, nome: "Fulano", email: "fulano@gmail.com"}
@@ -7,5 +9,43 @@ module.exports = {
     getAllUsers : function(req, res, next){
         res.setHeader("content-type", "application/json");
         res.status(200).json(users)
+    },
+    getUserById : function(req, res, next){
+        let id = req.params.id;
+        let result = users.filter(elemento => elemento.id == id);
+
+        if(result.length != 0){
+            return res.status(200).json(result);
+
+        }else{
+            return res.status(404).json({error: "Usuário não encontrado!"})
+        }
+    },
+    addUser : function(req, res, next){
+        let nome = req.body.name;
+        let email = req.body.email;
+        let user = {
+            id: ++id,
+            nome: nome,
+            email: email
+        }
+        
+        users.push(user);
+
+        return res.status(201).json(user);
+    },
+    deleteUser : function(req, res, next) {
+        let id = req.params.id;
+        for(i = 0; i < users.length; i ++){
+            let user = users[i];
+
+            if(user.id == id){
+                users.splice(i, 1);
+                return res.status(200).json({
+                    msg: "Usuário Removido!"
+                })
+            }
+        }
+        return res.status(404).json({error: "Usuário não encontrado!"})
     }
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,5 +4,8 @@ var router = express.Router();
 let controller = require('../controllers/userController')
 /* GET users listing. */
 router.get('/', controller.getAllUsers);
+router.get('/users/:id', controller.getUserById);
+router.post('/users', controller.addUser);
+router.delete('/users/:id', controller.deleteUser);
 
 module.exports = router;
